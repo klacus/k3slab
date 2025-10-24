f94dccf5ed03e7895986718d83550293f83a455d
Laszlo Kohl <laszlo.jobs@gmail.com>
2025-10-24 16:42:49 -0400

    added basic argocd

--- argocd/deploy.sh
+++ argocd/deploy.sh
@@ -3,5 +3,6 @@
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
+# We terminate the TLS at the gateway, so the ArgoCD service can be run in insecure mode.
 kubectl -n argocd patch cm argocd-cmd-params-cm --type merge --patch-file ./argocd-cmd-params-cm.yaml
 
