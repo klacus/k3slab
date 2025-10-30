a200d9be55ed48e8d83d428df2356f2be3443411
Laszlo Kohl <laszlo.jobs@gmail.com>
2025-10-30 11:29:57 -0400

    argocd chart or simple install

--- argocd/deploy.sh
+++ argocd/deploy.sh
@@ -8,3 +8,18 @@ kubectl -n argocd patch cm argocd-cmd-params-cm --type merge --patch-file ./argo
 
 kubectl apply -f ./gateway.yaml
 
+# # Chart location: https://artifacthub.io/packages/helm/argo/argo-cd
+# # Download Helm chart
+# echo "Downloading helm chart ..."
+# CHARTURL="https://github.com/argoproj/argo-helm/releases/download/argo-cd-9.0.5/argo-cd-9.0.5.tgz"
+# curl -O -L ${CHARTURL}
+
+# # Extracting helm chart files.
+# echo "Removing old Helm chart files ..."
+# rm -R ./argo-cd
+# echo "Extracting new Helm chart files ..."
+# tar -xvzf ./argo-cd-9.0.5.tgz
+
+# echo "Installing Helm chart from local folder ..."
+# helm install argo-cd ./argo-cd --namespace argocd --create-namespace --wait -f ./values-custom.yaml
+
